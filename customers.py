1d3b45fbed5796524f40b1d8edf2b758627d085b
unknown <yaacovana@gmail.com>
2023-10-19 19:56:17 +0300

    using blueprint

---
+++ customers.py
@@ -0,0 +1,43 @@
+import json
+from flask import  Blueprint, request
+from models import db, Customer
+
+customers = Blueprint('customers', __name__, url_prefix= '/customers' )
+
+@customers.route('/get',  methods = ['GET'])
+def get_customers():
+    res=[]
+    for cust in Customer.query.all():
+        res.append({"addr":cust.addr,"city":cust.city,"id":cust.id,"name":cust.name})
+    return  (json.dumps(res))
+
+@customers.route('/customers',  methods = ['POST'])
+def add_customer():
+    data = request.json
+    print(data)
+    newCustomer = Customer(data["name"], data["city"], data["addr"])
+    db.session.add(newCustomer)
+    db.session.commit()
+    return (data)
+   
+    
+@customers.route('/customers/<id>',  methods = ['PUT'])
+def upd_cust(id):
+    data = request.json
+    upd_row = Customer.query.filter_by(id=id).first()
+    if upd_row:
+        upd_row.city = data['city']
+        upd_row.name = data["name"]
+        upd_row.addr = data["addr"]
+        db.session.commit()
+        return {'upd': 'true'}
+    return {'upd': 'false'}
+
+@customers.route('/customers/<id>', methods = ['DELETE'])
+def delete(id):
+    del_row = Customer.query.filter_by(id=id).first()
+    if del_row:
+        db.session.delete(del_row)
+        db.session.commit()
+        return {'del': 'true'}
+    return {'del': 'false'}
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from app import app
+
+db = SQLAlchemy(app)
+
+
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    id = db.Column('student_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    addr = db.Column(db.String(200))
+    
+    def __init__(self, name, city, addr):
+        self.name = name
+        self.city = city
+        self.addr = addr
+
+with app.app_context():
+    db.create_all()
