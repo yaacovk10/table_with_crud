7c5564357684a5b44b221106ea2b16ec55e7ea30
unknown <yaacovana@gmail.com>
2023-10-23 23:00:53 +0300

    update __init__.py according to new blueprint customers and loans

--- project/__init__.py
+++ project/__init__.py
@@ -7,10 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customers.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.sqlite3'
 app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
 from project.customers import customers
+from project.loans import loans
+app.register_blueprint(loans)
 app.register_blueprint(customers)
